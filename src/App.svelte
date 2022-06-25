afc6c4633ebea5f7df2554fe96874c661b00f4d0
OAALLTECHINFO <102165261+OAALLTECHINFO@users.noreply.github.com>
2022-06-25 23:26:17 +0400

    Add files via upload

---
+++ src/App.svelte
@@ -0,0 +1,61 @@
+<html>
+ 
+<head>
+    <title>
+      Window moveBy() Method in HTML
+    </title>
+    <style>
+        h1 {
+            color: green;
+        }
+         
+        h2 {
+            font-family: Impact;
+        }
+         
+        body {
+            text-align: center;
+        }
+    </style>
+</head>
+ 
+<body>
+ 
+    <h1>GeeksforGeeks</h1>
+    <h2>Window moveBy() Method</h2>
+ 
+    <p>
+      For opening the Test window,
+      double click the "Open Window" button:
+    </p>
+ 
+    <button ondblclick="Opwind()">
+      Open Window
+    </button>
+    <br>
+ 
+    <p>
+      For moving the Test window,
+      double click the "Move Window" button:
+    </p>
+ 
+    <button ondblclick="Movewind()">
+      Move Window
+    </button>
+ 
+    <script>
+        function Opwind() {
+            TestWindow = window.open("", "TestWindow",
+                "width=400, height=400");
+            TestWindow.document.write("This is a Test Window.");
+        }
+ 
+        function Movewind() {
+            TestWindow.moveBy(200, 200);
+            TestWindow.focus();
+        }
+    </script>
+ 
+</body>
+ 
+</html>
